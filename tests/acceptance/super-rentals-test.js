064ed290469bbc59215815ceafb5d59c9a928ae6
akshay shinde <akshayshinde7289@gmail.com>
2021-11-09 20:23:06 +0530

    initial app intro

---
+++ tests/acceptance/super-rentals-test.js
@@ -0,0 +1,37 @@
+import { assert, module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | super rentals', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
+    assert.dom('h2').hasText('Welcome to Super Rentals!', 'got h2');
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('navigatin using the nav-bar', async function () {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+});
